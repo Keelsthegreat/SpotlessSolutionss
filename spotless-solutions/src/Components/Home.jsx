a9a2d84b396192704ce45cc10aa9173b3ec461d5
Akili Inniss <rasheemkhan@rasheems-mbp.mynetworksettings.com>
2023-06-19 20:31:13 -0400

    homepage nav

--- spotless-solutions/src/Components/Home.jsx
+++ spotless-solutions/src/Components/Home.jsx
@@ -4,8 +4,10 @@ function Home() {
   return (
     <div>
        <div className='topDiv'>
-       <h1 className='HomeHeader'>Happiness Starts With a Clean Home</h1>
-       <img src='pexels-jean-van-der-meulen-1454806.jpg'></img>
+        <div className='headerDiv'>
+       <h1 className='homeHeader'>Happiness Starts With a Clean Home</h1>
+        </div>
+       <img className='homeHeaderPic' src='https://cdn.discordapp.com/attachments/692019678482006016/1120475569570725935/pexels-jean-van-der-meulen-1454806.jpg' alt=''/>
         </div> 
     </div>
   )
