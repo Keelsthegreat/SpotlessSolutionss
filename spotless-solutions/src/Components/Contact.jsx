18fb66428678aad33a6aaf5c56ae23935671062c
Akili Inniss <rasheemkhan@rasheems-mbp.mynetworksettings.com>
2023-06-26 21:40:38 -0400

    added contact and services page

--- spotless-solutions/src/Components/Contact.jsx
+++ spotless-solutions/src/Components/Contact.jsx
@@ -1,9 +1,26 @@
+
 import React from 'react'
 
 function Contact() {
   return (
     <div>
         <h3>Contact Us Now!</h3>
+        <h4>You can fill out this form and we will give you a custom quote with more detailed information. If you need to call us please reach out to us at *blank*</h4>
+       
+       <div>
+        <h3> Name</h3>
+        <input type='text' id='name'  name='name' placeholder='Jane'/>
+        <h3>Phone Number</h3>
+        <input type='text' id='number' min='10' max='10' placeholder='(123)-456-7890'/>
+        <h3>Email</h3>
+        <input type='text'placeholder='Janedoe@example.com'/>
+        <h3>Select a Service</h3>
+       <label for = 'select a service'>Select a Service:</label>
+       <select name='services' id='services'>
+        <option value='flatrate'>Flat Rate</option>
+        <option value='hourly'>Hourly</option>
+       </select>
+       </div>
     </div>
   )
 }
--- spotless-solutions/src/Components/Services.jsx
+++ spotless-solutions/src/Components/Services.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 function Services() {
   return (
-    <div>Services</div>
+    <div>
+        <h1>Our Services</h1>
+        <h3>See our cleaning services below!</h3>
+
+        <div>
+        <img src='' alt=''/>
+        <h3>Standard Cleaning</h3>
+        </div>
+        
+        <div>
+        <img src='' alt=''/>
+        <h3>Deep Cleaning</h3>
+        <h4>The deep clean for busy homeowners who want their to-do list empty & house spotless. <a href=''>See what's included</a></h4>
+        </div>
+
+        <div>
+        <img src='' alt=''/>
+        <h3>Moving In/Moving Out Cleaning</h3>
+        <h4>The move-in/move-out cleaning will have your new or old home spotless and ready for moving day.</h4>
+        </div>
+        <button>Book Online</button>
+
+    </div>
   )
 }
 
